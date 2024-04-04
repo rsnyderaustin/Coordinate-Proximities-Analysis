4b02744815d183589e2b95c55aecf3683fef2d3a
Snyder <rsnyder.austin@gmail.com>
2024-04-04 12:01:42 -0500

    Update output_handling.py

--- io_handling/output_handling.py
+++ io_handling/output_handling.py
@@ -13,11 +13,22 @@ def output_dfs_to_file(output_path, dataframes: dict):
             df.insert(loc=0, column='index', value=index_col)
         df_list = list(dataframes.values())
         df_concatted = pd.concat(df_list)
-        df_concatted.to_csv(output_path, index=False)
-        logging.info("Output DataFrame to .csv file at '{output_path}'")
-    elif extension in '.xlsx':
-        with pd.ExcelWriter(output_path) as writer:
-            for name, df in dataframes.items():
-                df.to_excel(writer, sheet_name=name, index=False)
-                logging.info(f"Output DataFrame to Excel sheet name '{name}'")
-        logging.info(f"Output DataFrame to .xlsx at '{output_path}'")
+
+    output_success = False
+    while not output_success:
+        try:
+            if extension in '.csv':
+                df_concatted.to_csv(output_path, index=False)
+                logging.info("Output DataFrame to .csv file at '{output_path}'")
+            elif extension in '.xlsx':
+                with pd.ExcelWriter(output_path) as writer:
+                    for name, df in dataframes.items():
+                        df.to_excel(writer, sheet_name=name, index=False)
+                        logging.info(f"Output DataFrame to Excel sheet name '{name}'")
+                logging.info(f"Output DataFrame to .xlsx at '{output_path}'")
+            else:
+                raise ValueError(f"Extension is not one of accepted values (.csv, .xlsx)\nExtension: '{extension}'")
+        except PermissionError:
+            print(f"Attempting to write to file {output_path}, but the file is currently open.\n"
+                  f"Please close the file, then press any key to continue.")
+            input()
